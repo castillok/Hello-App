2bca3d5743bbaa12f945bbad4db23e9a05ae6677
karla castillo <kacastillo1@me.com>
2022-11-13 18:50:49 -0600

    Saving Initial IF conditions

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func helloButtonPressed(_ sender: UIButton) {
+        let greeting1 = "Hello"
+        let greeting2 = "Hola"
+        let greeting3 = "Ni Hao"
+        
+        if helloLabel.text == greeting1 {helloLabel.text = greeting2 }
+        else if helloLabel.text == greeting2 {helloLabel.text = greeting3 }
+        else {helloLabel.text = greeting1}
+    }
+    
 }
 
